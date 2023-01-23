d19e59c3d07be755a7a38e4cb9c646d7788950b1
mariananieton <rm94141@fiap.com.br>
2023-01-23 18:17:25 -0300

    Criando classe ProdutoDao com método cadastrar + construtor que recebe um EntityManager

---
+++ src/main/java/br/com/alura/loja/dao/ProdutoDao.java
@@ -0,0 +1,18 @@
+package br.com.alura.loja.dao;
+
+import br.com.alura.loja.modelo.Produto;
+
+import javax.persistence.EntityManager;
+
+public class ProdutoDao {
+
+	private EntityManager entityManager;
+
+	public ProdutoDao(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+
+	public void cadastrar(Produto produto){
+		this.entityManager.persist(produto);
+	}
+}
