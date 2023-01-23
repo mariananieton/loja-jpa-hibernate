e07942f8ba7530dd15c82aa9b822dcd98aea2bab
mariananieton <rm94141@fiap.com.br>
2023-01-23 18:45:42 -0300

    Transformando Categoria numa classe e mapeando-a

--- src/main/java/br/com/alura/loja/modelo/Categoria.java
+++ src/main/java/br/com/alura/loja/modelo/Categoria.java
@@ -1,8 +1,44 @@
 package br.com.alura.loja.modelo;
 
-public enum Categoria {
+import javax.persistence.*;
 
-	CELULARES,
-	INFORMATICA,
-	LIVROS;
+@Entity
+@Table(name = "T_LOJA_CATEGORIAS")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "id_Sequence")
+	@SequenceGenerator(name = "id_Sequence", sequenceName = "SQ_LOJA_CATEGORIA", allocationSize = 1)
+	@Column(name = "ID_CATEGORIA")
+	private Long id;
+	@Column(name = "NM_CATEGORIA")
+	private String nome;
+
+	public Categoria(String nome) {
+		this.nome = nome;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public String toString() {
+		return "Categoria{" +
+				"id=" + id +
+				", nome='" + nome + '\'' +
+				'}';
+	}
 }
