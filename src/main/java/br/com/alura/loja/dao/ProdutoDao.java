5e35f594baf0293256b4404c3be78b76a6e75311
mariananieton <rm94141@fiap.com.br>
2023-01-23 19:36:22 -0300

    Adicionando métodos de busca por id e buscar todos os registros

--- src/main/java/br/com/alura/loja/dao/ProdutoDao.java
+++ src/main/java/br/com/alura/loja/dao/ProdutoDao.java
@@ -3,6 +3,7 @@ package br.com.alura.loja.dao;
 import br.com.alura.loja.modelo.Produto;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 public class ProdutoDao {
 
@@ -14,5 +15,23 @@ public class ProdutoDao {
 
 	public void cadastrar(Produto produto){
 		this.entityManager.persist(produto);
+	}
+
+	public void atualizar(Produto produto) {
+		this.entityManager.merge(produto);
+	}
+
+	public void remover(Produto produto) {
+		produto = entityManager.merge(produto);
+		this.entityManager.remove(produto);
+	}
+
+	public Produto buscarPorId(Long id) {
+		return entityManager.find(Produto.class, id);
+	}
+
+	public List<Produto> buscarTodos() {
+		String jpql = "SELECT p FROM Produto p";
+		return entityManager.createQuery(jpql, Produto.class).getResultList();
 	}
 }
