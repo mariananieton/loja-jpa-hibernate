7c106ce82a4c9b19e1b2330a46c7e9aa97bb316b
mariananieton <rm94141@fiap.com.br>
2023-01-23 16:55:22 -0300

    Cadastrando um Produto utilizando EntityManager

---
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -0,0 +1,23 @@
+package br.com.alura.loja.testes;
+
+import br.com.alura.loja.modelo.Produto;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.math.BigDecimal;
+
+public class CadastroDeProduto {
+
+	public static void main(String[] args) {
+		Produto celular = new Produto();
+		celular.setNome("Xiaomi Redmi");
+		celular.setDescricao("Muito legal");
+		celular.setPreco(new BigDecimal("800"));
+
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("loja");
+		EntityManager entityManager = factory.createEntityManager();
+		entityManager.persist(celular);
+	}
+
+}
